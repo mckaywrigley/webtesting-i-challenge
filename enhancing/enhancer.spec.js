e948ae1fb32a2564efb20d16b77dc5cb2552e6c7
Mckay <mckaywrigley@gmail.com>
2019-05-11 17:04:00 -0700

    creates repair test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,7 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require("./enhancer.js");
 // test away!
+describe("Repair item.", () => {
+  it("Takes item object and sets durability property to 100.", () => {
+    expect(repair({ durability: 50 })).toEqual({ durability: 100 });
+  });
+});
