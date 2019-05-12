01778bedd420adcfbc95047e888c8166899618da
Mckay <mckaywrigley@gmail.com>
2019-05-11 17:06:58 -0700

    minor changes to syntax

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,13 @@
 const { succeed, fail, repair, get } = require("./enhancer.js");
 // test away!
+const item = { name: "Ray Gun", durability: 80, enhancement: 50 };
+
 describe("Repair item.", () => {
   it("Takes item object and sets durability property to 100.", () => {
-    expect(repair({ durability: 50 })).toEqual({ durability: 100 });
+    expect(repair(item)).toEqual({
+      name: "Ray Gun",
+      durability: 100,
+      enhancement: 50
+    });
   });
 });
